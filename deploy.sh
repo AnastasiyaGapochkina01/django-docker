15429c5e38da788626b3cf69605eb6792b4016de
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2021-08-05 18:35:03 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DEPLOY_USER='gitlab-runner'
+DEPLOY_HOST='192.168.56.103'
+PROJECT_PATH='/opt/deploy/django-docker'
+
+ssh -o StrictHostKeyChecking=no ${DEPLOY_USER}@${DEPLOY_HOST} "cd ${PROJECT_PATH} && \
+                                                               git checkout master && \
+                                                               git pull && \
+                                                               docker-compose up -d"
